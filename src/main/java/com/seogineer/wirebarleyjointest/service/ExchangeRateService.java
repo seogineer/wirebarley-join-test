4cfbf8d104eb527e1df2a051e951cc221cde5677
seogineer <dgseo8981@gmail.com>
2022-05-01 15:05:57 +0900

    NPE 처리

--- src/main/java/com/seogineer/wirebarleyjointest/service/ExchangeRateService.java
+++ src/main/java/com/seogineer/wirebarleyjointest/service/ExchangeRateService.java
@@ -7,6 +7,7 @@ import com.seogineer.wirebarleyjointest.dto.SubscribeResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -21,24 +22,34 @@ public class ExchangeRateService {
         Mono<SubscribeResponse> subscribeResponseMono = subscribeFromCurrencyLayer(currencyCode);
 
         return subscribeResponseMono.flatMap(subscribeResponse -> {
+            if(!subscribeResponse.getSuccess()){
+                return Mono.error(new SubscribeException());
+            }
+
             ExchangeRateResponse exchangeRateResponse = new ExchangeRateResponse();
-            exchangeRateResponse.setUSDKRW(subscribeResponse.getQuotes().getUSDKRW());
-            exchangeRateResponse.setUSDJPY(subscribeResponse.getQuotes().getUSDJPY());
-            exchangeRateResponse.setUSDPHP(subscribeResponse.getQuotes().getUSDPHP());
+
+            if(subscribeResponse.getQuotes().getUSDKRW() != null){
+                exchangeRateResponse.setUSDKRW(subscribeResponse.getQuotes().getUSDKRW());
+            } else if(subscribeResponse.getQuotes().getUSDJPY() != null){
+                exchangeRateResponse.setUSDJPY(subscribeResponse.getQuotes().getUSDJPY());
+            } else if(subscribeResponse.getQuotes().getUSDPHP() != null){
+                exchangeRateResponse.setUSDPHP(subscribeResponse.getQuotes().getUSDPHP());
+            }
+
             return Mono.just(exchangeRateResponse);
         });
     }
 
     private Mono<SubscribeResponse> subscribeFromCurrencyLayer(String currencyCode){
         return webClient.mutate()
-            .baseUrl("http://api.currencylayer.com")
-            .build()
-            .get()
-            .uri("/live?access_key=e71a30b85dbfc4f147207e56463d5c4c&format=1&&currencies={currencyCode}", currencyCode)
-            .retrieve()
-            .onStatus(HttpStatus::is4xxClientError, response -> Mono.just(new BadRequestException()))
-            .onStatus(HttpStatus::is5xxServerError, response -> Mono.just(new SubscribeException()))
-            .bodyToMono(SubscribeResponse.class)
-            .timeout(Duration.ofSeconds(20));
+                    .baseUrl("http://api.currencylayer.com")
+                    .build()
+                    .get()
+                    .uri("/live?access_key=e71a30b85dbfc4f147207e56463d5c4c&format=1&&currencies={currencyCode}", currencyCode)
+                    .retrieve()
+                    .onStatus(HttpStatus::is4xxClientError, response -> Mono.just(new BadRequestException()))
+                    .onStatus(HttpStatus::is5xxServerError, response -> Mono.just(new SubscribeException()))
+                    .bodyToMono(SubscribeResponse.class)
+                    .timeout(Duration.ofSeconds(20));
     }
 }
